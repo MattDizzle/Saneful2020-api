e9119f13b818851d3b78a9d60c18d4fe796d861a
Matthew Rougely <matthewrougelysr@gmail.com>
2021-06-06 15:36:43 -0700

    removed config implementation

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
     PORT: process.env.SERVER_PORT,
-    NODE_ENV: process.env.NODE_ENV ,
+    NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
   };
-} else if(process.env.NODE_ENV == "test") {
+} else if (process.env.NODE_ENV == "test") {
   module.exports = {
     PORT: process.env.SERVER_PORT,
-    NODE_ENV: process.env.NODE_ENV ,
+    NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.LOCAL_DATABASE_URL,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
   };
 }
