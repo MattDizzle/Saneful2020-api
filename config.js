feafd596d8d86a0ac414b2b46dbf9dea894df222
Matthew Rougely <matthewrougelysr@gmail.com>
2021-06-06 15:08:23 -0700

    adjusting env variables

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV == "production") {
   module.exports = {
     PORT: process.env.SERVER_PORT,
     NODE_ENV: process.env.NODE_ENV ,
-    DATABASE_URL: process.env.PROD_DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET
   };
 } else if(process.env.NODE_ENV == "test") {
