aae6d9a8baf596217cfc8864b2c88525a6893e23
Matthew Rougely <matthewrougelysr@gmail.com>
2021-06-06 13:02:01 -0700

    just trying something

--- postgrator-config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  'migrationDirectory': 'migrations',
-  'driver': 'pg',
-  'connectionString': (process.env.NODE_ENV === 'production') 
-  ? process.env.PROD_DATABASE_URL
-  : process.env.LOCAL_DATABASE_URL,
-};
