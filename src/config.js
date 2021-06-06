11f7db8bcfaa58ff0ca4de86e75fc2fa9e6e54cf
Matthew Rougely <matthewrougelysr@gmail.com>
2021-06-06 10:37:44 -0700

    Updated configurations

--- src/config.js
+++ src/config.js
@@ -1,6 +1,27 @@
-module.exports = {
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/saneful',
-  JWT_SECRET: process.env.JWT_SECRET || '106816c9-bcb5-4a05-9d51-4dda16a357c3',
-};
+// module.exports = {
+//   PORT: process.env.PORT,
+//   NODE_ENV: process.env.NODE_ENV,
+//   DATABASE_URL: process.env.DATABASE_URL,
+//   JWT_SECRET: process.env.JWT_SECRET,
+//   PROD_DATABASE_URL: process.env.PROD_DATABASE_URL,
+// };
+
+if (process.env.NODE_ENV == "production") {
+  module.exports = {
+    PORT: process.env.PROD_PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.PROD_DATABASE_URL,
+    JWT_SECRET: process.env.PROD_JWT_SECRET,
+    HOST: process.env.PROD_HOST,
+    DATABASE: process.env.PROD_DATABASE,
+    USER: process.env.PROD_USER,
+    PASSWORD: process.env.PROD_PASSWORD,
+  };
+} else {
+  module.exports = {
+    PORT: process.env.LOCAL_PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.LOCAL_DATABASE_URL,
+    JWT_SECRET: process.env.LOCAL_JWT_SECRET,
+  };
+}
