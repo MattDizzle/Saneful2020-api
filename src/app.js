65831235b13ef823e832b35bc16fcb112cef5ec2
Matthew Rougely <matthewrougelysr@gmail.com>
2021-06-06 16:59:28 -0700

    changed error handling

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'production server error' + error.message } };
+    response = { error: { message: 'production server error:  ' + error.message } };
   } else {
     console.error(error);
     response = { message: error.message, error };
